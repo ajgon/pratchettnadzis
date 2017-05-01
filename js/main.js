b7a5312f08919da8d272e0c6455b14f788e9861b
Igor Rzegocki <igor@rzegocki.pl>
2017-05-01 19:29:16 +0200

    fixed preloader

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@
         } else {
           populateRandomItem();
         }
-        $('.preloader-wrapper, .data-card').toggleClass('hide');
+        $('.preloader-container, .data-card').toggleClass('hide');
       });
     };
 
